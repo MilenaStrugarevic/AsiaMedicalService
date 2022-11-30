2090589c3974f4206e5af68f3ff55507b2e6b7a5
QA36 <QA36@DESKTOP-QPQFVHR>
2022-11-30 19:37:15 +0100

    Add new Tests

---
+++ MedicalService/MedicalService/Tests/LoginTest.cs
@@ -0,0 +1,50 @@
+﻿using MedicalService.Driver;
+using MedicalService.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalService.Tests
+{
+    internal class LoginTest
+    {
+        LoginPage loginPage;
+        string Message = "Login failed! Please ensure the username and password are valid.";
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+        }
+        [Test]
+        public void TC01_InvalidUserName()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("Milena","ThisIsNotAPassword");
+            Assert.That(Message, Is.EqualTo(loginPage.UserNoLogin.Text));
+        }
+        [Test]
+        public void TC02_EnteringInvalidPasword_ShouldNotBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("John Doe", "WrongPassword");
+            Assert.That(Message, Is.EqualTo(loginPage.UserNoLogin.Text));
+        }
+        [Test]
+        public void TC03_EnterNoData_ShouldNotBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("", "");
+            Assert.That(Message, Is.EqualTo(loginPage.UserNoLogin.Text));
+        }
+    }
+}
